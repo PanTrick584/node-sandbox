d0715cefad5f6f44c84e8bc9732e79831779b495
Patryk <chodacki.pc@gmail.com>
2024-09-25 11:20:18 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    if(req.url === "/") return res.end("Welcome to Homepage")
+    if(req.url === "/about") return res.end("About page")
+
+    return res.end("Oops, there is no site you are looking for");
+    // res.write("Welcome to our homepage");
+    // res.end();
+})
+
+server.listen(5000);
