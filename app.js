be8aad0eb7daf5b24370e189180a18bd52689c11
Patryk <chodacki.pc@gmail.com>
2024-09-30 08:51:56 +0200

    another chapter

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
+require("dotenv").config();
+require("express-async-errors")
+// async errors
+
 const express = require("express");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products")
 const app = express();
-const tasks = require("./routes/tasks");
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/hello", (req, res) => {
-    res.send("task manager app")
-})
+const homePage = "<h1>Home Page</h1><a href='/api/v1/products'>Go to Products</a>"
+// routes
+app.get('/', (req, res) => res.send(homePage))
+app.use('/api/v1/products', productsRouter)
+// app.use('/api/v1/products')
 
+// product route
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-app.use("/api/v1/tasks", tasks)
+const PORT = process.env.PORT || 3000;
 
-const port = 5000;
-app.listen(port, () => console.log("Server listens on port : 5000..."));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT, () => console.log("Server is listening on port : 6000"))
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
