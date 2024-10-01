f9564dda13641704e56da4dfcb8b39da6f227a17
Patryk <chodacki.pc@gmail.com>
2024-10-01 07:31:52 +0200

    change for work

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 const connectDB = require("./db/connect");
 const productsRouter = require("./routes/products")
 const app = express();
-
+const print = console.log;
 app.use(express.json());
 
+print("siema");
 const homePage = "<h1>Home Page</h1><a href='/api/v1/products'>Go to Products</a>"
 // routes
 app.get('/', (req, res) => res.send(homePage))
@@ -21,12 +22,12 @@ app.use('/api/v1/products', productsRouter)
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3300;
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(PORT, () => console.log("Server is listening on port : 6000"))
+        app.listen(PORT, () => console.log("Server is listening on port : 3300"))
         
     } catch (error) {
         console.log(error);
--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,35 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({msg: 'products testing route static'})
+        const products = await Product.find({}).sort("-name price")
+    res.status(200).json({ products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products testing route'})
+    const {featured, company, name,  sort} = req.query;
+    const queryObject = {};
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+    if (featured) {
+        queryObject.featured = featured === "true" ?? false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+
+    let result = Product.find(queryObject)
+
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createAt')
+    }
+    const products = await result;
+
+    res.status(200).json({ products, nbHits: products.length})
 }
 
 module.exports = {
