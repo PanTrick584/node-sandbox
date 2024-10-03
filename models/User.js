a509c3fd5c307b99eef43497206f8c9d9f481ad9
Patryk Chodacki <patryk.chodacki@lppsa.com>
2024-10-03 16:05:37 +0200

    another one

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -9,16 +10,20 @@ const UserSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: [true, "Please provide name"],
+        required: [true, "Given email is already registered"],
         match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email'],
         unique: true
     },
     password: {
         type: String,
         required: [true, "Please provide password"],
-        minlength: 6,
-        maxlength: 20
+        minlength: 6
     }
+})
+
+UserSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 module.exports = mongoose.model("User", UserSchema)
